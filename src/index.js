602997641125a556796a9036d17e7d01fb528bc1
igibbs@myersinfosys.com <igibbs@myersinfosys.com>
2017-06-06 14:00:58 -0400

    site is up and running on webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// Set up your application entry point here...
+// .src/index.js
+
+// Babel polyfill will emulate a full
+// ES2015 environemnt so we can enjoy goodies like
+// Promises
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+
+render(
+  <Router routes={routes} history={browserHistory} />,
+  document.getElementById('app')
+);
